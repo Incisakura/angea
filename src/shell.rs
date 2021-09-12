d9173cd7d52b2e628702309e396b852bdb31d815
Incisakura <incisakura@icloud.com>
2021-09-12 13:01:03 +0800

    refactor: dbus message implementation

--- src/shell.rs
+++ src/shell.rs
@@ -1,10 +1,13 @@
 use std::env;
+use std::ffi::{CStr, CString};
+use std::mem;
+use std::os::raw::c_void;
+use std::os::unix::io::RawFd;
+use std::ptr;
 use std::process::{Command, Stdio};
+use std::result::Result;
 
-use dbus::arg::OwnedFd;
-use dbus::ffidisp::{BusType, Connection};
-use dbus::message::Message;
-use dbus::Error;
+use libdbus_sys::*;
 
 use crate::ptyfwd::PTYForward;
 
@@ -14,8 +17,11 @@ pub fn enter() {
         return;
     }
 
-    let owned_fd = get_master().expect("");
-    let mut f = PTYForward::new(owned_fd.into_fd());
+    let owned_fd = match unsafe { get_master() } {
+        Ok(fd) => fd,
+        Err(e) => unsafe { panic!("{}", CStr::from_ptr(e.message).to_str().unwrap()) },
+    };
+    let mut f = PTYForward::new(owned_fd);
     f.wait();
 }
 
@@ -27,7 +33,31 @@ fn is_inside() -> bool {
         .map_or(false, |s| s.success())
 }
 
-fn get_master() -> Result<OwnedFd, Error> {
+unsafe fn get_master() -> Result<RawFd, DBusError> {
+    unsafe fn append_string(iter: *mut DBusMessageIter, value: &str) {
+        let value = CString::new(value).unwrap();
+        dbus_message_iter_append_basic(
+            iter,
+            DBUS_TYPE_STRING,
+            &value.as_ptr() as *const _ as *const c_void,
+        );
+    }
+    
+    unsafe fn append_array_string(
+        m: *mut DBusMessage,
+        iter: *mut DBusMessageIter,
+        values: Vec<String>,
+    ) {
+        let signature = CString::new("s").unwrap();
+        let mut i = mem::zeroed();
+        dbus_message_iter_init_append(m, &mut i);
+        dbus_message_iter_open_container(iter, DBUS_TYPE_ARRAY, signature.as_ptr(), &mut i);
+        for value in values {
+            append_string(&mut i, value.as_str());
+        }
+        dbus_message_iter_close_container(iter, &mut i);
+    }
+
     let args: Vec<String> = Vec::new();
     let envs: Vec<String> = env::vars()
         .filter_map(|(k, v)| {
@@ -37,18 +67,51 @@ fn get_master() -> Result<OwnedFd, Error> {
             None
         })
         .collect();
-    let c = Connection::get_private(BusType::System)?;
-    let mut m = Message::new_method_call(
-        "org.freedesktop.machine1",
-        "/org/freedesktop/machine1",
-        "org.freedesktop.machine1.Manager",
-        "OpenMachineShell",
-    )
-    .unwrap();
-    // host | user | path | args | envs
-    m.append_all((".host", "", "", args, envs));
-    let r = c.send_with_reply_and_block(m, 3000)?;
-    let (owned_fd, _): (OwnedFd, String) = r.read2()?;
-    // return ownedfd to keep ownership
-    Ok(owned_fd)
+
+    // Init connection
+    let mut e = DBusError {
+        name: ptr::null(),
+        message: ptr::null(),
+        dummy: 0,
+        padding1: ptr::null(),
+    };
+    dbus_error_init(&mut e);
+    let conn = dbus_bus_get_private(DBusBusType::System, &mut e);
+    if conn.is_null() {
+        return Err(e);
+    }
+
+    // new method call
+    let dest = CString::new("org.freedesktop.machine1").unwrap();
+    let path = CString::new("/org/freedesktop/machine1").unwrap();
+    let iface = CString::new("org.freedesktop.machine1.Manager").unwrap();
+    let method = CString::new("OpenMachineShell").unwrap();
+    let m = dbus_message_new_method_call(
+        dest.as_ptr(),
+        path.as_ptr(),
+        iface.as_ptr(),
+        method.as_ptr(),
+    );
+
+    // Append args
+    let mut iter: DBusMessageIter = mem::zeroed();
+    dbus_message_iter_init_append(m, &mut iter);
+    append_string(&mut iter, ".host");
+    append_string(&mut iter, "");
+    append_string(&mut iter, "");
+    append_array_string(m as *mut _, &mut iter, args);
+    append_array_string(m as *mut _, &mut iter, envs);
+
+    // Send message and recive reply
+    let m = dbus_connection_send_with_reply_and_block(conn as *mut _, m as *mut _, 3000, &mut e);
+    if m.is_null() {
+        return Err(e);
+    }
+
+    // Get result
+    let mut i: DBusMessageIter = mem::zeroed();
+    dbus_message_iter_init(m, &mut i);
+    let mut fd: RawFd = mem::zeroed();
+    dbus_message_iter_get_basic(&mut i, &mut fd as *mut _ as *mut c_void);
+    Ok(fd)
 }
