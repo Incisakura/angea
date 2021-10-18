2dfe57b66a9d982b7ef4f6287befd9c82ea087e3
Incisakura <incisakura@icloud.com>
2021-10-18 17:54:10 +0800

    refactor: pointer generics cast

--- src/shell.rs
+++ src/shell.rs
@@ -1,4 +1,4 @@
-use std::ffi::{c_void, CString};
+use std::ffi::{c_void, CStr, CString};
 use std::mem;
 use std::mem::MaybeUninit;
 use std::os::raw::c_char;
@@ -205,15 +205,15 @@ impl PTYForward {
     }
 }
 
+const SD_BUS_TYPE_ARRAY: c_char = 'a' as c_char;
+
+const SD_BUS_TYPE_VARIANT: c_char = 'v' as c_char;
+
+const SD_BUS_TYPE_STRUCT: c_char = 'r' as c_char;
+
+#[rustfmt::skip]
 /// D-Bus call to spawn a shell service in systemd
 unsafe fn dbus(user: &str, slave: &str) -> Result<()> {
-    /// Convert sd_bus_* return value to `nix::Result`
-    fn assert(v: i32) -> Result<()> {
-        if v < 0 {
-            return Err(Errno::from_i32(-v));
-        }
-        Ok(())
-    }
     // Prepare arguments
     let mut args = [
         "/bin/bash\0".as_ptr() as *mut c_char,
@@ -239,117 +239,60 @@ unsafe fn dbus(user: &str, slave: &str) -> Result<()> {
     assert(sd_bus_message_new_method_call(
         bus,
         message.as_mut_ptr(),
-        "org.freedesktop.systemd1\0".as_ptr() as _,
-        "/org/freedesktop/systemd1\0".as_ptr() as _,
-        "org.freedesktop.systemd1.Manager\0".as_ptr() as _,
-        "StartTransientUnit\0".as_ptr() as _,
+        char("org.freedesktop.systemd1\0"),
+        char("/org/freedesktop/systemd1\0"),
+        char("org.freedesktop.systemd1.Manager\0"),
+        char("StartTransientUnit\0"),
     ))?;
     let message = message.assume_init();
 
     // Append message arguments
     assert(sd_bus_message_append(
         message,
-        "ss\0".as_ptr() as _,
-        service.as_ptr() as *const _ as *const c_void,
-        "fail\0".as_ptr() as *const _ as *const c_void,
+        char("ss\0"),
+        void(service.as_c_str()),
+        void("fail\0"),
     ))?;
 
     // Enter a(sv)
-    assert(sd_bus_message_open_container(
-        message,
-        'a' as c_char,
-        "(sv)\0".as_ptr() as _,
-    ))?;
+    assert(sd_bus_message_open_container(message, SD_BUS_TYPE_ARRAY, char("(sv)\0")))?;
     assert(sd_bus_message_append(
         message,
-        "(sv)(sv)(sv)(sv)(sv)(sv)\0".as_ptr() as _,
-        "WorkingDirectory\0".as_ptr() as *const _ as *const c_void,
-        "s\0".as_ptr() as *const _ as *const c_void,
-        "~\0".as_ptr() as *const _ as *const c_void,
-        "StandardInput\0".as_ptr() as *const _ as *const c_void,
-        "s\0".as_ptr() as *const _ as *const c_void,
-        "tty\0".as_ptr() as *const _ as *const c_void,
-        "StandardOutput\0".as_ptr() as *const _ as *const c_void,
-        "s\0".as_ptr() as *const _ as *const c_void,
-        "tty\0".as_ptr() as *const _ as *const c_void,
-        "StandardError\0".as_ptr() as *const _ as *const c_void,
-        "s\0".as_ptr() as *const _ as *const c_void,
-        "tty\0".as_ptr() as *const _ as *const c_void,
-        "TTYPath\0".as_ptr() as *const _ as *const c_void,
-        "s\0".as_ptr() as *const _ as *const c_void,
-        slave.as_ptr() as *const _ as *const c_void, // slave
-        "User\0".as_ptr() as *const _ as *const c_void,
-        "s\0".as_ptr() as *const _ as *const c_void,
-        user.as_ptr() as *const _ as *const c_void, // user
+        char("(sv)(sv)(sv)(sv)(sv)(sv)\0"),
+        void("WorkingDirectory\0"), void("s\0"), void("~\0"),
+        void("StandardOutput\0"),   void("s\0"), void("tty\0"),
+        void("StandardInput\0"),    void("s\0"), void("tty\0"),
+        void("StandardError\0"),    void("s\0"), void("tty\0"),
+        void("TTYPath\0"),          void("s\0"), void(slave.as_c_str()),
+        void("User\0"),             void("s\0"), void(user.as_c_str()),
     ))?;
 
     // Environment
-    assert(sd_bus_message_open_container(
-        message,
-        'r' as _,
-        "sv\0".as_ptr() as _,
-    ))?;
-    assert(sd_bus_message_append(
-        message,
-        "s\0".as_ptr() as _,
-        "Environment\0".as_ptr() as *const _ as *const c_void,
-    ))?;
-    assert(sd_bus_message_open_container(
-        message,
-        'v' as _,
-        "as\0".as_ptr() as _,
-    ))?;
+    assert(sd_bus_message_open_container(message, SD_BUS_TYPE_STRUCT, char("sv\0")))?;
+    assert(sd_bus_message_append(message, char("s\0"), void("Environment\0")))?;
+    assert(sd_bus_message_open_container(message, SD_BUS_TYPE_VARIANT, char("as\0")))?;
     assert(sd_bus_message_append_strv(message, envs.as_mut_ptr()))?;
     assert(sd_bus_message_close_container(message))?;
     assert(sd_bus_message_close_container(message))?;
 
     // ExecStart
-    assert(sd_bus_message_open_container(
-        message,
-        'r' as c_char,
-        "sv\0".as_ptr() as _,
-    ))?;
-    assert(sd_bus_message_append(
-        message,
-        "s\0".as_ptr() as _,
-        "ExecStart\0".as_ptr() as *const _ as *const c_void,
-    ))?;
-    assert(sd_bus_message_open_container(
-        message,
-        'v' as c_char,
-        "a(sasb)\0".as_ptr() as _,
-    ))?;
-    assert(sd_bus_message_open_container(
-        message,
-        'a' as c_char,
-        "(sasb)\0".as_ptr() as _,
-    ))?;
-    assert(sd_bus_message_open_container(
-        message,
-        'r' as c_char,
-        "sasb\0".as_ptr() as _,
-    ))?;
-    assert(sd_bus_message_append(
-        message,
-        "s\0".as_ptr() as _,
-        args[0] as *const _ as *const c_void,
-    ))?;
+    assert(sd_bus_message_open_container(message, SD_BUS_TYPE_STRUCT, char("sv\0")))?;
+    assert(sd_bus_message_append(message, char("s\0"), void("ExecStart\0")))?;
+    assert(sd_bus_message_open_container(message, SD_BUS_TYPE_VARIANT, char("a(sasb)\0")))?;
+    assert(sd_bus_message_open_container(message, SD_BUS_TYPE_ARRAY, char("(sasb)\0")))?;
+    assert(sd_bus_message_open_container(message, SD_BUS_TYPE_STRUCT, char("sasb\0")))?;
+    assert(sd_bus_message_append(message, char("s\0"), void(args[0])))?;
     assert(sd_bus_message_append_strv(message, args.as_mut_ptr()))?;
-    assert(sd_bus_message_append(message, "b\0".as_ptr() as _, 1))?; // 1 stands for `true`
+    assert(sd_bus_message_append(message, char("b\0"), 1))?; // 1 stands for `true`
     assert(sd_bus_message_close_container(message))?;
     assert(sd_bus_message_close_container(message))?;
     assert(sd_bus_message_close_container(message))?;
     assert(sd_bus_message_close_container(message))?;
-
     // Exit a(sv)
     assert(sd_bus_message_close_container(message))?;
 
     // Auxiliary
-    assert(sd_bus_message_append(
-        message,
-        "a(sa(sv))\0".as_ptr() as _,
-        0,
-    ))?;
+    assert(sd_bus_message_append(message, char("a(sa(sv))\0"), 0))?;
 
     // Send message
     assert(sd_bus_call(
@@ -365,4 +308,48 @@ unsafe fn dbus(user: &str, slave: &str) -> Result<()> {
     sd_bus_message_unref(message);
 
     Ok(())
+}
+
+/// Convert sd_bus_* return value to `nix::Result`
+fn assert(v: i32) -> Result<()> {
+    if v < 0 {
+        return Err(Errno::from_i32(-v));
+    }
+    Ok(())
+}
+
+/// Convert tring like type to `*const c_char`
+fn char<T: StrLike>(v: T) -> *const c_char {
+    v.as_char_ptr()
+}
+
+/// Convert tring like type to `*const c_void`
+fn void<T: StrLike>(v: T) -> *const c_void {
+    v.as_void_ptr()
+}
+
+trait StrLike: Sized {
+    fn as_char_ptr(&self) -> *const c_char;
+
+    fn as_void_ptr(&self) -> *const c_void {
+        self.as_char_ptr().cast()
+    }
+}
+
+impl StrLike for &CStr {
+    fn as_char_ptr(&self) -> *const c_char {
+        self.as_ptr()
+    }
+}
+
+impl StrLike for &'static str {
+    fn as_char_ptr(&self) -> *const c_char {
+        self.as_ptr().cast()
+    }
+}
+
+impl StrLike for *mut c_char {
+    fn as_char_ptr(&self) -> *const c_char {
+        *self
+    }
 }
